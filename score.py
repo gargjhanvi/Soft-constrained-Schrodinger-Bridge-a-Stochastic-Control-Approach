4d44f8e31666af31b6995910fb5f948dc5e9a846
Jhanvi Garg <89818286+gargjhanvi@users.noreply.github.com>
2024-02-20 10:59:58 -0600

    Update score.py

--- score.py
+++ score.py
@@ -1,29 +1,21 @@
 import subprocess
 import argparse
 
-# Create an ArgumentParser object
 parser = argparse.ArgumentParser()
 
-# Define the command-line argument
 parser.add_argument("--beta", type=str, help="Regularization parameter in soft contrained Schrodinger bridge problem")
-
-# Parse the command-line arguments
 args = parser.parse_args()
 
-# Access the value of the '--var' argument
 beta_value = args.beta
 
-# Your script can now use 'var_value' as needed
 print(f"Value of 'beta': {beta_value}")
 
-# List of .py files to trigger along with variable names and values
 py_files_to_run = [
     ('score_ref.py', {'beta': beta_value}),
     ('score_obj.py', {'beta': beta_value})
 
 ]
 
-#Loop through the list and run each .py file with its variables
 for py_file, variables in py_files_to_run:
     try:
         # Construct the command to run the .py file with variables
